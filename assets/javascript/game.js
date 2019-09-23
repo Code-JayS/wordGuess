d6c34f343f4b83e05767c2a2e3e1760fb3502149
J. Smith <joe.a.smith@gmail.com>
2019-09-23 18:08:44 -0500

    OMG game logic works now!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,79 @@
-
-
-// var round = 0;
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-/* word list of answers */
+var validEntry = "abcdefghijklmnopqrstuvwxyz".split("");
 var words = ["brains", "zombie", "creepy", "pumpkin", "skull", "candy", "rotten",];
 /* Randomly chooses a choice from the words array. This is the answer.*/
 var word = words[Math.floor(Math.random() * words.length)];
 //stores the word letters as array
 var wordBank = [];
-for (var i = 0; i < word.length; i++) {
-  wordBank[i] = " __ ";
-}
-var LettersLeft = word.length;
-
+var lettersToWin = word.length;
+var wrongBank = [];
 // var letters = computerGuess.split("");
-var brainsLeft = 10;
+var brainsLeft = 1;
 var output = "";
 var inputLetter = "";
 console.log(word)
-console.log(display)
+console.log(word.length)
 console.log(output)
 console.log(brainsLeft)
 console.log(wordBank)
-function progress (){
-document.getElementById("display").innerHTML = wordBank.join(" ");
+
+// updates progress
+function progress() {
+  document.getElementById("display").innerHTML = wordBank.join(" ");
+  document.getElementById("help").innerHTML = wrongBank.join(" ")
 }
-function lose(){
+// updates display on loss
+function lose() {
   document.getElementById("display").innerHTML = "You were deafeated by the Humans!";
 }
-function win(){
+// updates display on win
+function win() {
   document.getElementById("display").innerHTML = "The word was " + word.toUpperCase() + " You ate many smart brains";
 }
-//get letter value  
-document.onkeyup = function (event) {
+
+//sets the wordbank up with dashes 
+for (var i = 0; i < word.length; i++) {
+  wordBank[i] = " __ ";
+}
+// records user input 
+window.addEventListener("keyup", squish, false);
+
+
+
+
+
+//get letter value 
+function squish(event) {
   inputLetter = event.key;
   console.log(inputLetter)
-  //Make game work logic
-  for (var j = 0; j < LettersLeft; j++) {
-    if (word[j] === inputLetter) {
-      wordBank[j] = inputLetter;
-      //get word closer to win length
-      LettersLeft--;
-      progress()
-    }
-    
-    else if (LettersLeft === 0){
-      win()
-    }
-    else {
-      brainsLeft--;
+  if (word.indexOf(inputLetter) !== -1) {
+    //   console.log (inputLetter)
+    for (var j = 0; j < word.length; j++) {
+
+
+      if (word[j] === inputLetter) {
+        wordBank[j] = inputLetter;
+        lettersToWin--;
+        progress()
+        if (lettersToWin === 0) {
+          win()
+        }
+        console.log(lettersToWin)
+        // console.log(brainsLeft)
+      }
     }
   }
+  // else if ((word.indexOf(inputLetter) || wrongBank.indexOf(inputLetter)) !== -1) {
+  else if (wrongBank.indexOf(inputLetter) === -1) {
+    wrongBank.push(inputLetter);
+    brainsLeft--;
+    console.log(brainsLeft)
+    progress()
+    if (brainsLeft === 0) {
+      lose();
+    }
+
+   
+  }
 }
+
+
