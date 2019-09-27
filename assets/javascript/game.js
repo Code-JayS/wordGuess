fb276561f95c9c2c1cff146cab289194769a57bc
J. Smith <joe.a.smith@gmail.com>
2019-09-26 21:27:18 -0500

    Updated html to include guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+var wins = 0
+let loses = 0
+
 var validEntry = "abcdefghijklmnopqrstuvwxyz".split("");
 var words = ["brains", "zombie", "creepy", "pumpkin", "skull", "candy", "rotten",];
 /* Randomly chooses a choice from the words array. This is the answer.*/
@@ -7,73 +10,97 @@ var wordBank = [];
 var lettersToWin = word.length;
 var wrongBank = [];
 // var letters = computerGuess.split("");
-var brainsLeft = 1;
-var output = "";
+var brainsLeft = 7;
 var inputLetter = "";
-console.log(word)
-console.log(word.length)
-console.log(output)
-console.log(brainsLeft)
-console.log(wordBank)
+
+
+
+
+
 
 // updates progress
 function progress() {
   document.getElementById("display").innerHTML = wordBank.join(" ");
-  document.getElementById("help").innerHTML = wrongBank.join(" ")
+  document.getElementById("help").innerHTML = wrongBank.join(" ");
+  document.getElementById("lives").innerHTML = brainsLeft;
+
+
+
 }
 // updates display on loss
 function lose() {
   document.getElementById("display").innerHTML = "You were deafeated by the Humans!";
+  initializeGame();
+  loses++;
+  document.getElementById("humans").innerHTML = "HUMANS " + loses;
 }
 // updates display on win
 function win() {
   document.getElementById("display").innerHTML = "The word was " + word.toUpperCase() + " You ate many smart brains";
+  initializeGame()
+  wins++;
+  document.getElementById("zombies").innerHTML = "ZOMBIES " + wins;
 }
 
-//sets the wordbank up with dashes 
-for (var i = 0; i < word.length; i++) {
-  wordBank[i] = " __ ";
-}
-// records user input 
-window.addEventListener("keyup", squish, false);
+
+
+function initializeGame() {
+  var words = ["brains", "zombie", "creepy", "pumpkin", "skull", "candy", "rotten",];
+  var word = words[Math.floor(Math.random() * words.length)];
+  wordBank = [];
+  let lettersToWin = word.length;
+  wrongBank = [];
+  brainsLeft = 7;
+  inputLetter = "";
+
+
+  //sets the wordbank up with dashes 
+  for (var i = 0; i < word.length; i++) {
+    wordBank[i] = " __ ";
+  }
+  // records user input 
+  window.addEventListener("keyup", squish, false);
 
 
 
 
 
-//get letter value 
-function squish(event) {
-  inputLetter = event.key;
-  console.log(inputLetter)
-  if (word.indexOf(inputLetter) !== -1) {
-    //   console.log (inputLetter)
-    for (var j = 0; j < word.length; j++) {
+  //get letter value 
+  function squish(event) {
+    inputLetter = event.key;
+    console.log(inputLetter);
+    inputLetter.toLowerCase();
+    if (validEntry.indexOf(inputLetter) !== -1) {
+      if (word.indexOf(inputLetter) !== -1) {
+        //   console.log (inputLetter)
+        for (var j = 0; j < word.length; j++) {
 
 
-      if (word[j] === inputLetter) {
-        wordBank[j] = inputLetter;
-        lettersToWin--;
-        progress()
-        if (lettersToWin === 0) {
-          win()
+          if (word[j] === inputLetter) {
+            wordBank[j] = inputLetter;
+            lettersToWin--;
+            progress();
+            if (lettersToWin === 0) {
+              win();
+            }
+            console.log(lettersToWin)
+            // console.log(brainsLeft)
+          }
         }
-        console.log(lettersToWin)
-        // console.log(brainsLeft)
+      }
+      // else if ((word.indexOf(inputLetter) || wrongBank.indexOf(inputLetter)) !== -1) {
+      else if (wrongBank.indexOf(inputLetter) === -1) {
+        wrongBank.push(inputLetter);
+        brainsLeft--;
+        console.log(brainsLeft)
+        progress();
+        if (brainsLeft === 0) {
+          lose();
+        }
+
+
       }
     }
   }
-  // else if ((word.indexOf(inputLetter) || wrongBank.indexOf(inputLetter)) !== -1) {
-  else if (wrongBank.indexOf(inputLetter) === -1) {
-    wrongBank.push(inputLetter);
-    brainsLeft--;
-    console.log(brainsLeft)
-    progress()
-    if (brainsLeft === 0) {
-      lose();
-    }
-
-   
-  }
 }
-
-
+initializeGame()
